2edba98f9e668adb2e6eed110e91cfae4305b0dd
Jaime Fernández Moreno <jaimefrm93@gmail.com>
2025-02-11 21:15:35 +0100

    analytics and vercel ssr adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  })
 });
