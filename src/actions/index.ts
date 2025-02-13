0a61f88b555a3ba9845c6c233ca98b7e7c39ecd1
Jaime Fernández Moreno <jaimefrm93@gmail.com>
2025-02-13 19:07:42 +0100

    himare web contact page

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -11,17 +11,19 @@ export const server = {
       const email = formData.get("email");
       const message = formData.get("message");
       const service = formData.getAll("service");
+      const formSource = formData.get("formSource");
 
       const { data, error } = await resend.emails.send({
         from: 'HIMARE <himare@loveactually.es>',
         to: ['quitinavarrom@gmail.com'],
-        subject: 'HIMARE-FORMULARIO SERVICIOS',
+        subject: `HIMARE-FORMULARIO ${formSource}`,
         html: `${name} se ha puesto en contacto con Himare a través del formulario de servicios. Sus datos son los siguientes:<br><br>
         Nombre: ${name}<br><br>
         Email: ${email}<br><br>
         Servicios que le interesan: ${service.join(', ')}<br><br>
         Ha dejado este mensaje:<br>
-        ${message}
+        ${message}<br><br>
+        Este formulario se ha rellenado en la página: ${formSource}
         `
       });
 
