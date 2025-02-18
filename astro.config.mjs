328b4f4be1a52ca1a2defd71647b487bcff15067
Jaime Fernández Moreno <jaimefrm93@gmail.com>
2025-02-18 21:47:00 +0100

    Added seo and sitemap config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import vercel from '@astrojs/vercel';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -13,5 +15,7 @@ export default defineConfig({
     webAnalytics: {
       enabled: true,
     },
-  })
+  }),
+
+  integrations: [sitemap()]
 });
